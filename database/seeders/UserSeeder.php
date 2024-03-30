535a20c906d0f25f4e0699719e496f0f59e52168
Kah Wai <wai.chong@outlook.com>
2024-03-31 00:57:58 +0800

    update user password

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,24 +17,24 @@ class UserSeeder extends Seeder
         $user = User::updateOrCreate(['email'=>'admin@oxyminds.com'],[
             'name' => 'Admin',
             'email' => 'admin@oxyminds.com',
-            'password' => Hash::make('qweqwe11'),
+            'password' => Hash::make('123456'),
         ]);
         $user->assignRole('admin');
 
         $backend = [[
             'name' => 'John Doe',
             'email' => 'johndow@mail.com',
-            'password' => Hash::make('qweqwe11'),
+            'password' => Hash::make('123456'),
         ],
         [
             'name' => 'John Doe 2',
             'email' => 'johndow2@mail.com',
-            'password' => Hash::make('qweqwe11'),
+            'password' => Hash::make('123456'),
         ],
         [
             'name' => 'John Doe 3',
             'email' => 'johndow3@mail.com',
-            'password' => Hash::make('qweqwe11'),
+            'password' => Hash::make('123456'),
         ],
         ];
         foreach ($backend as $key => $value) {
@@ -45,17 +45,17 @@ class UserSeeder extends Seeder
         $frontend = [[
             'name' => 'John Doe 4',
             'email' => 'johndow4@mail.com',
-            'password' => Hash::make('qweqwe11'),
+            'password' => Hash::make('123456'),
         ],
         [
             'name' => 'John Doe 5',
             'email' => 'johndow5@mail.com',
-            'password' => Hash::make('qweqwe11'),
+            'password' => Hash::make('123456'),
         ],
         [
             'name' => 'John Doe 6',
             'email' => 'johndow6@mail.com',
-            'password' => Hash::make('qweqwe11'),
+            'password' => Hash::make('123456'),
         ],
         ];
         foreach ($frontend as $key => $value) {
@@ -66,12 +66,12 @@ class UserSeeder extends Seeder
         $apps = [[
             'name' => 'John Doe 7',
             'email' => 'johndow7@mail.com',
-            'password' => Hash::make('qweqwe11'),
+            'password' => Hash::make('123456'),
         ],
         [
             'name' => 'John Doe 8',
             'email' => 'johndow8@mail.com',
-            'password' => Hash::make('qweqwe11'),
+            'password' => Hash::make('123456'),
         ],
         ];
         foreach ($apps as $key => $value) {
@@ -82,12 +82,12 @@ class UserSeeder extends Seeder
         $pc = [[
             'name' => 'John Doe 9',
             'email' => 'johndow9@mail.com',
-            'password' => Hash::make('qweqwe11'),
+            'password' => Hash::make('123456'),
         ],
         [
             'name' => 'John Doe10',
             'email' => 'johndow10@mail.com',
-            'password' => Hash::make('qweqwe11'),
+            'password' => Hash::make('123456'),
         ],
         ];
         foreach ($pc as $key => $value) {
